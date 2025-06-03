2d64e40b672b0b4e916b452cbadeddf7d55c8e76
rashimarasangi <rashimarasangi200@gmail.com>
2025-06-03 19:21:37 +0530

    sidebar is added

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Sidebar.css'
+import {assets} from'../../assets/assets'
+
+const Sidebar = () => {
+  return (
+    <div className='Sidebar'>
+      <div className="top">
+        <img src={assets.menu_icon} alt="" />
+        <div className="new-chat">
+            <img src={assets.plus_icon} alt="" />
+            <p>New Chat</p>
+        </div>
+        <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="reset-entry">
+                <img src={assets.message_icon} alt="" />
+                <p>What is react..</p>
+            </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="bottom-item reset-entry">
+            <img src={assets.question_icon} alt="" />
+            <p>Help</p>
+        </div>
+        <div className="bottom-item reset-entry">
+            <img src={assets.history_icon} alt="" />
+            <p>Activity</p>
+        </div>
+        <div className="bottom-item reset-entry">
+            <img src={assets.setting_icon} alt="" />
+            <p>Settings</p>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
